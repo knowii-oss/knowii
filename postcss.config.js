983b777baabd3792a605f63ef0c363cb1d18f668
Sebastien Dubois <seb@dsebastien.net>
2023-02-12 17:30:27 +0100

    Improved postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -3,10 +3,13 @@ const { join } = require('path');
 module.exports = {
   plugins: {
     'postcss-import': {},
+    // Reference: https://tailwindcss.com/docs/using-with-preprocessors
+    'tailwindcss/nesting': {},
     tailwindcss: {
       config: join(__dirname, 'tailwind.config.js'),
     },
     'postcss-flexbugs-fixes': {},
+    // Reference: https://github.com/csstools/postcss-preset-env
     'postcss-preset-env': {
       autoprefixer: {
         flexbox: 'no-2009',
@@ -14,8 +17,9 @@ module.exports = {
       stage: 3,
       features: {
         'custom-properties': false,
+        // Reference: https://tailwindcss.com/docs/using-with-preprocessors
+        'nesting-rules': false,
       },
     },
-    //autoprefixer: {},
   },
 };
