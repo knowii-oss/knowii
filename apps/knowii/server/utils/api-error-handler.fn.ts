d3e8ee34223af1681c42dd9ea654e4156dea4e1a
Sebastien Dubois <seb@dsebastien.net>
2023-02-12 16:42:28 +0100

    Added api error handler

---
+++ apps/knowii/server/utils/api-error-handler.fn.ts
@@ -0,0 +1,22 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+/**
+ * Api error handle
+ * @param err
+ * @param _req
+ * @param res
+ * @param _next
+ */
+export const apiErrorHandler = async (
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  err: any,
+  _req: NextApiRequest,
+  res: NextApiResponse,
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  _next: any
+): Promise<void> => {
+  // FIXME improve
+  console.log(err);
+  res.status(500).end('Internal server error');
+};
