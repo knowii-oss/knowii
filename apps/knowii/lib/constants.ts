0cac3a2a8d52718913b62ea8600d4330d736ac4b
Sebastien Dubois <seb@dsebastien.net>
2023-02-12 17:28:07 +0100

    Added constants

---
+++ apps/knowii/lib/constants.ts
@@ -0,0 +1,12 @@
+/**
+ * Utils
+ */
+export const IS_BROWSER = typeof window !== 'undefined';
+export const IS_PROD = process.env.NODE_ENV === 'production';
+export const IS_TEST = process.env.NODE_ENV === 'test';
+
+/**
+ * Meta
+ */
+export const BUILD_TIME = new Date().toString();
+export const BUILD_TIMESTAMP = Date.now();
