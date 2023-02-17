dedaca7b04840ecbfc2a5e523b28d447a6126222
Sebastien Dubois <seb@dsebastien.net>
2023-02-17 12:30:23 +0100

    Added auth utils

---
+++ apps/knowii/lib/client/auth.ts
@@ -0,0 +1,23 @@
+import { useUser } from '@supabase/auth-helpers-react';
+import { useRouter } from 'next/router';
+import { useMemo } from 'react';
+import { redirectPath } from '../../config/auth';
+
+export function useUserName() {
+  const user = useUser();
+  const userName = useMemo(() => {
+    return user?.user_metadata.full_name || user?.email;
+  }, [user]);
+
+  return userName;
+}
+
+export function useAuthRedirectUrl(path = redirectPath) {
+  const { locale, defaultLocale } = useRouter();
+
+  const baseUrl = useMemo(() => (typeof window !== 'undefined' ? window.location.origin : '/'), []);
+  const localePath = useMemo(() => (locale === defaultLocale ? '' : `/${locale}`), [locale, defaultLocale]);
+  const formattedPath = useMemo(() => (path.startsWith('/') ? path : `/${path}`), [path]);
+
+  return useMemo(() => `${baseUrl}${localePath}${formattedPath}`, [localePath, formattedPath, baseUrl]);
+}
