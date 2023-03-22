93e8afe04e9c93e811e61d73a4abc3bd2ab91727
Sebastien Dubois <seb@dsebastien.net>
2023-03-22 10:40:32 +0100

    Added support for next.js router for Storybook stories

--- libs/client-ui/.storybook/main.js
+++ libs/client-ui/.storybook/main.js
@@ -1,7 +1,7 @@
 module.exports = {
   core: { builder: 'webpack5' },
   stories: ['../src/lib/**/*.stories.mdx', '../src/lib/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-essentials', '@nrwl/react/plugins/storybook'],
+  addons: ['@storybook/addon-essentials', '@nrwl/react/plugins/storybook', 'storybook-addon-next-router'],
   // Customize the Webpack configuration
   // References
   // - https://storybook.js.org/docs/react/builders/webpack#extending-storybooks-webpack-config
--- libs/client-ui/.storybook/preview.js
+++ libs/client-ui/.storybook/preview.js
@@ -1,1 +1,9 @@
 import './tailwind-imports.css';
+
+import { RouterContext } from 'next/dist/shared/lib/router-context';
+
+export const parameters = {
+  nextRouter: {
+    Provider: RouterContext.Provider,
+  },
+};
