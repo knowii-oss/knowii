1c7fb1cc03a938df5d61e07c92ea56b919e271e6
Sebastien Dubois <seb@dsebastien.net>
2023-03-24 09:19:55 +0100

    Improved metadata for SEO

--- apps/knowii/pages/_document.tsx
+++ apps/knowii/pages/_document.tsx
@@ -2,6 +2,29 @@ import React, { ReactElement } from 'react';
 import Document, { Html, Head, Main, NextScript, DocumentContext, DocumentInitialProps } from 'next/document';
 import { ColorModeScript } from '@chakra-ui/react';
 import { customTheme } from '../chakra-ui.config';
+import { IS_BROWSER } from '@knowii/common';
+import Script from 'next/script';
+
+// eslint-disable-next-line  @typescript-eslint/no-var-requires
+const siteAuthor = require('../../../libs/common/src/lib/metadata.json').author;
+// eslint-disable-next-line  @typescript-eslint/no-var-requires
+const siteDescription = require('../../../libs/common/src/lib/metadata.json').description;
+// eslint-disable-next-line  @typescript-eslint/no-var-requires
+const siteDomain = require('../../../libs/common/src/lib/metadata.json').domain;
+// eslint-disable-next-line  @typescript-eslint/no-var-requires
+const siteName = require('../../../libs/common/src/lib/metadata.json').siteName;
+// eslint-disable-next-line  @typescript-eslint/no-var-requires
+const siteTitle = require('../../../libs/common/src/lib/metadata.json').title;
+
+const siteMicrodata = {
+  '@context': 'https://schema.org',
+  '@type': 'WebSite',
+  name: siteName,
+  alternateName: siteTitle,
+  description: siteDescription,
+  publisher: siteAuthor,
+  url: IS_BROWSER ? window.location.origin : '',
+};
 
 /**
  * Custom HTML document
@@ -17,8 +40,20 @@ export default class CustomDocument extends Document<{
     return (
       <Html lang="en">
         <Head>
+          <meta charSet="utf-8" />
+          <meta name="robots" content="index,follow,max-image-preview:large" />
+
           {this.props.styleTags}
           {/* Preload the fonts */}
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700;1,900&display=swap"
+            rel="stylesheet"
+          />
           <link rel="preload" href="/fonts/AvenirNext-Regular.woff2" as="font" crossOrigin="anonymous" />
           <link rel="preload" href="/fonts/AvenirNext-Bold.woff2" as="font" crossOrigin="anonymous" />
           <link rel="preload" href="/fonts/AvenirNext-BoldItalic.woff2" as="font" crossOrigin="anonymous" />
@@ -36,9 +71,17 @@ export default class CustomDocument extends Document<{
           <link href="/icons/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png" />
           <link href="/icons/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png" />
           {/* Safari Pinned Tab Icon: https://developer.apple.com/library/archive/documentation/AppleApplications/Reference/SafariWebContent/pinnedTabs/pinnedTabs.html */}
-          <link color="#4a9885" href="/static/icons/favicon.svg" rel="mask-icon" />
-
-          <meta content="/static/browserconfig.xml" name="msapplication-config" />
+          <link color="#4a9885" href="/icons/favicon.svg" rel="mask-icon" />
+          <script
+            defer
+            data-domain={siteDomain}
+            data-api="https://blue-bar-dsebastien-19fd.developassion.workers.dev/api/v1/event"
+            src="https://blue-bar-dsebastien-19fd.developassion.workers.dev/content/script.js"
+          ></script>
+          <meta content="/browserconfig.xml" name="msapplication-config" />
+          <Script id="site-microdata-script" type="application/ld+json">
+            {JSON.stringify(siteMicrodata)}
+          </Script>
         </Head>
         <body className="loading">
           <ColorModeScript initialColorMode={customTheme.config.initialColorMode} />
