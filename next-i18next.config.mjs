5674a1b955d89bfe494af989f77e2f3649e1459c
Sebastien Dubois <seb@dsebastien.net>
2023-03-31 17:55:31 +0200

    Tried adapting the locale path for vercel (SSR issue)

--- next-i18next.config.mjs
+++ next-i18next.config.mjs
@@ -2,6 +2,8 @@
 
 import { root } from './helpers.mjs';
 
+const isRunningOnLocalhost = !!process.env['NEXT_PUBLIC_IS_RUNNING_ON_LOCALHOST'];
+
 const languages = ['en', 'fr'];
 
 /**
@@ -23,6 +25,11 @@ export const i18nConfig = {
   },
   // To avoid issues when deploying to Vercel
   // Reference: https://github.com/i18next/next-i18next/blob/master/examples/simple/next-i18next.config.js
-  localePath: typeof window === 'undefined' ? root('apps/knowii/public/locales') : '/locales',
+  localePath:
+    typeof window === 'undefined' && isRunningOnLocalhost
+      ? root('apps/knowii/public/locales')
+      : typeof window === 'undefined'
+      ? './locales'
+      : '/locales',
   reloadOnPrerender: process.env.NODE_ENV === 'development',
 };
