33398b55911c3049a004299cbfc32a528dd8e5d5
Sebastien Dubois <seb@dsebastien.net>
2023-03-31 13:42:31 +0200

    Updated scripts

--- libs/server/src/lib/stripe-admin.ts
+++ libs/server/src/lib/stripe-admin.ts
@@ -3,7 +3,7 @@ import Stripe from 'stripe';
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? '', {
   apiVersion: '2022-11-15',
   appInfo: {
-    name: 'supastarter demo', // FIXME set correct name
+    name: 'knowii',
     version: '0.1.0',
   },
 });
