3cc4fd9fbd0dfc1b8415eced4939f78959553f99
Sebastien Dubois <seb@dsebastien.net>
2023-03-31 18:03:34 +0200

    Tried something else for translations

--- next-i18next.config.mjs
+++ next-i18next.config.mjs
@@ -25,11 +25,6 @@ export const i18nConfig = {
   },
   // To avoid issues when deploying to Vercel
   // Reference: https://github.com/i18next/next-i18next/blob/master/examples/simple/next-i18next.config.js
-  localePath:
-    typeof window === 'undefined' && isRunningOnLocalhost
-      ? root('apps/knowii/public/locales')
-      : typeof window === 'undefined'
-      ? './locales'
-      : '/locales',
+  localePath: typeof window === 'undefined' && isRunningOnLocalhost ? root('apps/knowii/public/locales') : '/locales',
   reloadOnPrerender: process.env.NODE_ENV === 'development',
 };
