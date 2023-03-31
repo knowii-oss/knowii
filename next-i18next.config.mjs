048d8e12d3bb2e3648d074860289bc22f2f7767c
Sebastien Dubois <seb@dsebastien.net>
2023-03-31 17:24:48 +0200

    Updated path to locales for vercel

--- next-i18next.config.mjs
+++ next-i18next.config.mjs
@@ -23,6 +23,6 @@ export const i18nConfig = {
   },
   // To avoid issues when deploying to Vercel
   // Reference: https://github.com/i18next/next-i18next/blob/master/examples/simple/next-i18next.config.js
-  localePath: typeof window === 'undefined' ? root('apps/knowii/public/locales') : '/locales',
+  localePath: typeof window === 'undefined' ? root('apps/knowii/public/locales') : './public/locales',
   reloadOnPrerender: process.env.NODE_ENV === 'development',
 };
