40ec31540f9793583c1d84855f33431bfffb1951
Sebastien Dubois <seb@dsebastien.net>
2023-04-13 14:46:40 +0200

    Re-enabled output file tracing

--- apps/knowii/next.config.mjs
+++ apps/knowii/next.config.mjs
@@ -3,11 +3,11 @@
 
 import { withNx } from '@nrwl/next/plugins/with-nx.js';
 import { i18nConfig } from '../../i18n.config.mjs';
-//import path from 'path';
-//import { fileURLToPath } from 'url';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-//const __filename = fileURLToPath(import.meta.url);
-//const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 console.log(`> Building in "${process.env.NODE_ENV}" mode (NODE_ENV)`);
 
@@ -51,13 +51,12 @@ const nextConfig = {
      */
     ignoreBuildErrors: true,
   },
-  //output: 'standalone', // Enable Output File Tracing:  https://nextjs.org/docs/advanced-features/output-file-tracing
-  /*experimental: {
+  output: 'standalone', // Enable Output File Tracing:  https://nextjs.org/docs/advanced-features/output-file-tracing
+  experimental: {
     // This includes files from the monorepo base two directories up
     // Required for output file tracing
     outputFileTracingRoot: path.join(__dirname, '../../'),
   },
-   */
 };
 
 export default withNx(nextConfig);
