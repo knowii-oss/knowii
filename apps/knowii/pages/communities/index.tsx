010f9c1a87c34f3518e45da26ac9514113e7d695
Sebastien Dubois <seb@dsebastien.net>
2023-04-19 14:33:08 +0200

    Added communities page (blank), removed features/pricing. #237

---
+++ apps/knowii/pages/communities/index.tsx
@@ -0,0 +1,53 @@
+import { Box, Container, VStack } from '@chakra-ui/react';
+import { GetStaticProps } from 'next';
+import { useTranslations } from 'next-intl';
+import Layout from '../../components/layout/layout';
+import { PageHeader } from '@knowii/client-ui';
+import { i18nConfig } from '../../../../i18n.config.mjs';
+import { CustomPageProps } from '../_app';
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface CommunitiesPageProps {}
+
+export const getStaticProps: GetStaticProps<Partial<CustomPageProps>> = async (ctx) => {
+  const locale = ctx.locale ? ctx.locale : i18nConfig.i18n.defaultLocale;
+
+  const messages = (await import(`../../../../libs/common/src/lib/messages/${locale}.json`)).default;
+
+  const retVal: {
+    props: Partial<CustomPageProps & CommunitiesPageProps>;
+  } = {
+    props: {
+      messages,
+      // Note that when `now` is passed to the app, you need to make sure the
+      // value is updated from time to time, so relative times are updated. See
+      // https://next-intl-docs.vercel.app/docs/usage/configuration#global-now-value
+      now: new Date().getTime(),
+    },
+  };
+
+  return retVal;
+};
+
+export function CommunitiesPage(_props: CommunitiesPageProps) {
+  const t = useTranslations('communitiesPage');
+
+  return (
+    <Layout
+      customMeta={{
+        title: t('title'),
+      }}
+    >
+      <PageHeader title={t('title')} description={t('description')} />
+      <Box px={4} py={12}>
+        <Container maxW="5xl">
+          <VStack spacing={4} align="stretch">
+            {t('comingSoon')}
+          </VStack>
+        </Container>
+      </Box>
+    </Layout>
+  );
+}
+
+export default CommunitiesPage;
