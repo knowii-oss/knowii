2d23c70b8243621a5068206d2426c0ac75e215af
Sebastien Dubois <seb@dsebastien.net>
2023-05-03 08:53:27 +0200

    Updated nx

--- libs/client-ui/.storybook/main.js
+++ libs/client-ui/.storybook/main.js
@@ -1,7 +1,12 @@
 module.exports = {
-  core: { builder: 'webpack5' },
   stories: ['../src/lib/**/*.stories.mdx', '../src/lib/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-essentials', '@nrwl/react/plugins/storybook', 'storybook-addon-next-router', 'storybook-tailwind-dark-mode'],
+  addons: [
+    '@storybook/addon-essentials',
+    '@nx/react/plugins/storybook',
+    'storybook-addon-next-router',
+    'storybook-tailwind-dark-mode',
+    '@storybook/addon-mdx-gfm',
+  ],
   // Customize the Webpack configuration
   // References
   // - https://storybook.js.org/docs/react/builders/webpack#extending-storybooks-webpack-config
@@ -11,5 +16,12 @@ module.exports = {
       ...config.resolve.alias,
     };
     return config;
+  },
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {},
+  },
+  docs: {
+    autodocs: true,
   },
 };
--- libs/client-ui/.storybook/preview.js
+++
@@ -1,9 +0,0 @@
-import './tailwind-imports.css';
-
-import { RouterContext } from 'next/dist/shared/lib/router-context';
-
-export const parameters = {
-  nextRouter: {
-    Provider: RouterContext.Provider,
-  },
-};
