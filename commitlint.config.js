9595fdaa76364a89e7f6ffe8b1b4bfe27ded8222
Sebastien Dubois <seb@dsebastien.net>
2023-05-10 14:27:21 +0200

    build(all): added commitlint and commitizen

---
+++ commitlint.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+
+  /*
+   * Any rules defined here will override rules from @commitlint/config-conventional
+   Reference: https://commitlint.js.org/#/reference-rules
+   */
+  rules: {
+    'header-max-length': [1, 'always', 100],
+    'scope-enum': [2, 'always', ['all', 'a11y', 'code', 'deps', 'docs', 'i18n', 'qa', 'release', 'sec']],
+    'scope-case': [2, 'always', 'lowerCase'],
+  },
+};
