b3c02cda40ec95b2af5a25815ae66f8a27d815b5
Sebastien Dubois <seb@dsebastien.net>
2023-05-16 13:02:27 +0200

    feat(auth): added new commitlint scopes
    ISSUES CLOSED: n

--- commitlint.config.js
+++ commitlint.config.js
@@ -7,7 +7,7 @@ module.exports = {
    */
   rules: {
     'header-max-length': [1, 'always', 100],
-    'scope-enum': [2, 'always', ['all', 'a11y', 'code', 'deps', 'docs', 'i18n', 'qa', 'release', 'sec']],
+    'scope-enum': [2, 'always', ['all', 'a11y', 'code', 'deps', 'docs', 'i18n', 'qa', 'release', 'sec', 'ui', 'ux', 'auth']],
     'scope-case': [2, 'always', 'lowerCase'],
   },
 };
