b6cbdc9f8a239e7b85dca09dc8821967a7c9c068
Sebastien Dubois <seb@dsebastien.net>
2023-05-26 11:36:26 +0200

    feat(code): implement community creation API

---
+++ apps/knowii/pages/api/v1/communities/create.ts
@@ -0,0 +1,81 @@
+import { createServerSupabaseClient } from '@supabase/auth-helpers-nextjs';
+import { NextApiRequest, NextApiResponse } from 'next';
+import {
+  errorClientNotAuthenticated,
+  errorInternalServerError,
+  hasErrorMessage,
+  createCommunityRequestSchema,
+  CreateCommunityResponse,
+} from '@knowii/common';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== 'POST') {
+    res.setHeader('Allow', 'POST');
+    res.status(405).end('Method Not Allowed');
+    return;
+  }
+
+  const supabaseClient = createServerSupabaseClient({ req, res });
+
+  const {
+    data: { session },
+  } = await supabaseClient.auth.getSession();
+
+  if (!session) {
+    return res.status(401).json({
+      error: errorClientNotAuthenticated.code,
+      errorDescription: errorClientNotAuthenticated.description,
+    });
+  }
+
+  const validatedRequest = createCommunityRequestSchema.safeParse(req.body);
+
+  if (!validatedRequest.success) {
+    const responseBody: CreateCommunityResponse = {
+      error: validatedRequest.error.name,
+      errorDescription: validatedRequest.error.message,
+    };
+    return res.status(400).json(responseBody);
+  }
+
+  try {
+    //const prisma = new PrismaClient();
+
+    // const communitiesWithThatName = await prisma.communities.count({
+    //   where: {
+    //     name: nameToCheck,
+    //   },
+    // });
+    //
+    // const isNameAvailable = communitiesWithThatName === 0;
+    //
+    // if (isNameAvailable) {
+    //   console.log('The community name is available');
+    // } else {
+    //   console.log('The community name is not available');
+    // }
+    //
+    // const responseBody: IsCommunityNameAvailableResponse = {
+    //   isNameAvailable,
+    // };
+
+    return res.status(200).json({ cool: true });
+  } catch (err: unknown) {
+    if (hasErrorMessage(err)) {
+      console.warn(`Error while creating a new community: ${err.message}`);
+      res.status(500).json({
+        error: {
+          statusCode: 500,
+          errorDescription: errorInternalServerError.description,
+        },
+      });
+      return;
+    }
+
+    console.warn(`Error while creating a new community: ${err}`);
+    res.status(500).json({
+      error: errorInternalServerError.code,
+      errorDescription: errorInternalServerError.description,
+    });
+  }
+}
