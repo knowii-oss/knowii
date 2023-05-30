2471665d8203078297c3dce5072fe3e7e539cb58
Sebastien Dubois <seb@dsebastien.net>
2023-05-30 20:03:27 +0200

    chore(code): improved logging

--- libs/server/src/lib/dao/communities/index.ts
+++ libs/server/src/lib/dao/communities/index.ts
@@ -1,9 +1,12 @@
 import { Communities, PrismaClient } from '@prisma/client';
-import { CreateCommunityInput, createCommunityInputSchema, errorInputValidation } from '@knowii/common';
+import { CreateCommunityInput, createCommunityInputSchema, errorInputValidation, getLogger } from '@knowii/common';
 import { generateErrorMessage } from 'zod-error';
 import { errorMessageOptions } from '../../utils';
 
 export async function daoFnIsCommunityNameAvailable(name: string, prismaClient: PrismaClient): Promise<boolean> {
+  const logger = getLogger('communities', daoFnIsCommunityNameAvailable.name);
+
+  logger.debug('Checking if the following community name is available: %s', name);
   const communitiesWithThatName = await prismaClient.communities.count({
     where: {
       name,
@@ -13,15 +16,18 @@ export async function daoFnIsCommunityNameAvailable(name: string, prismaClient: 
   const isNameAvailable = communitiesWithThatName === 0;
 
   if (isNameAvailable) {
-    console.log('The community name is available');
+    logger.debug('The community name is available');
   } else {
-    console.log('The community name is not available');
+    logger.debug('The community name is not available');
   }
 
   return isNameAvailable;
 }
 
 export async function daoFnIsCommunitySlugAvailable(slug: string, prismaClient: PrismaClient): Promise<boolean> {
+  const logger = getLogger('communities', daoFnIsCommunitySlugAvailable.name);
+
+  logger.debug('Checking if the following community slug is available: %s', name);
   const communitiesWithThatSlug = await prismaClient.communities.count({
     where: {
       slug,
@@ -31,34 +37,44 @@ export async function daoFnIsCommunitySlugAvailable(slug: string, prismaClient: 
   const isSlugAvailable = communitiesWithThatSlug === 0;
 
   if (isSlugAvailable) {
-    console.log('The community slug is available');
+    logger.debug('The community slug is available');
   } else {
-    console.log('The community slug is not available');
+    logger.debug('The community slug is not available');
   }
 
   return isSlugAvailable;
 }
 
 export async function daoFnCreateCommunity(input: CreateCommunityInput, prismaClient: PrismaClient): Promise<Communities> {
-  console.log('Creating a new community: ', input);
+  const logger = getLogger('communities', daoFnCreateCommunity.name);
+
+  logger.info('Creating a new community: %s', input);
 
   const inputValidationResult = createCommunityInputSchema.safeParse(input);
 
   if (!inputValidationResult.success) {
     const errorMessage = generateErrorMessage(inputValidationResult.error.issues, errorMessageOptions);
-    console.warn(`${errorInputValidation.description}. Error(s) detected: `, errorMessage);
+    logger.warn(`${errorInputValidation.description}. Error(s) detected: %s`, errorMessage);
 
     // FIXME introduce specific error types
     throw new Error(errorMessage);
   }
 
-  const createdCommunity = await prismaClient.communities.create({
-    data: {
-      name: input.name,
-      description: input.description,
-      slug: input.slug,
-    },
-  });
+  try {
+    const createdCommunity = await prismaClient.communities.create({
+      data: {
+        name: input.name,
+        description: input.description,
+        slug: input.slug,
+      },
+    });
 
-  return createdCommunity;
+    logger.info('Community created successfully: %o', createdCommunity);
+
+    return createdCommunity;
+  } catch (err: unknown) {
+    logger.error('Failed to create the community: %o', err);
+    // FIXME improve error handling
+    throw err;
+  }
 }
--- libs/server/src/lib/dao/users/index.ts
+++ libs/server/src/lib/dao/users/index.ts
@@ -1,6 +1,10 @@
 import { PrismaClient } from '@prisma/client';
+import { getLogger } from '@knowii/common';
 
 export async function daoFnIsUsernameAvailable(username: string, prismaClient: PrismaClient): Promise<boolean> {
+  const logger = getLogger('communities', daoFnIsUsernameAvailable.name);
+
+  logger.debug('Checking if the following username is available: %s', username);
   const usersWithThatUsername = await prismaClient.users.count({
     where: {
       username,
