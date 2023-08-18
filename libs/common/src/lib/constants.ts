b7c946a284690778cdcdd55bf5c91654c11c0c8c
Sebastien Dubois <seb@dsebastien.net>
2023-08-18 22:50:19 +0200

    fix(all): fixed API_BASE_PATH usage to avoid // and needless redirects

--- libs/common/src/lib/constants.ts
+++ libs/common/src/lib/constants.ts
@@ -58,9 +58,9 @@ export const THEME_LIGHT = 'light';
 export const API_BASE_PATH = '/api/v1';
 export const API_COMMUNITIES_BASE_PATH = '/communities';
 export const API_USERS_BASE_PATH = '/users';
-export const API_USERNAME_AVAILABILITY_CHECK = `${API_BASE_PATH}/${API_USERS_BASE_PATH}/is-username-available`;
-export const API_COMMUNITY_NAME_AVAILABILITY_CHECK = `${API_BASE_PATH}/${API_COMMUNITIES_BASE_PATH}/is-community-name-available`;
-export const API_CREATE_NEW_COMMUNITY = `${API_BASE_PATH}/${API_COMMUNITIES_BASE_PATH}/create`;
+export const API_USERNAME_AVAILABILITY_CHECK = `${API_BASE_PATH}${API_USERS_BASE_PATH}/is-username-available`;
+export const API_COMMUNITY_NAME_AVAILABILITY_CHECK = `${API_BASE_PATH}${API_COMMUNITIES_BASE_PATH}/is-community-name-available`;
+export const API_CREATE_NEW_COMMUNITY = `${API_BASE_PATH}${API_COMMUNITIES_BASE_PATH}/create`;
 
 /**
  * URLs
@@ -70,7 +70,7 @@ export const APP_BASE_URL = '/app';
 export const ACCOUNT_URL = '/account';
 export const COMMUNITY_BASE_URL = '/community';
 export const COMMUNITIES_BASE_URL = '/communities';
-export const CREATE_COMMUNITY_URL = `${COMMUNITIES_BASE_URL}/create`;
+export const CREATE_COMMUNITY_URL = `${COMMUNITIES_BASE_URL}/create`; // TODO use in UI
 export const BLOG_BASE_URL = '/blog';
 export const PRIVACY_POLICY_URL = '/privacy';
 export const TERMS_OF_USE_URL = '/terms';
