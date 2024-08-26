eabae9bd63909890be65f48957e8440218f3cc97
Sébastien Dubois <sebastien.dubois@nbb.be>
2024-08-26 16:15:28 +0200

    style(all): extracted theme to a js file

--- libs/common/src/lib/constants.ts
+++ libs/common/src/lib/constants.ts
@@ -1,3 +1,6 @@
+/**
+ * Utils
+ */
 export const IS_BROWSER = typeof window !== 'undefined';
 export const IS_SERVER = typeof window === 'undefined';
 export const IS_PROD = process.env.NODE_ENV === 'production';
