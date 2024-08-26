11b99d0aa44e4f763692ab533613e9e5232d0998
Sébastien Dubois <sebastien.dubois@nbb.be>
2024-08-26 16:27:51 +0200

    style(all): added base layout

--- apps/app/src/app/layout.tsx
+++ apps/app/src/app/layout.tsx
@@ -1,9 +1,13 @@
-import './styles/global.scss';
+import './global.scss';
+
 import 'primereact/resources/themes/lara-light-blue/theme.css';
 import 'primeicons/primeicons.css';
 
+import ProgressBar from './components/layout/progress-bar';
+
 import { PrimeReactProvider } from 'primereact/api';
 import { Metadata } from 'next';
+import React from 'react';
 
 export const metadata: Metadata = {
   title: 'Knowii',
@@ -14,7 +18,19 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body>
-        <PrimeReactProvider value={{ ripple: true }}>{children}</PrimeReactProvider>
+        <main className="knowii-app-wrapper">
+          <PrimeReactProvider value={{ ripple: true }}>
+            <ProgressBar />
+            {/* FIXME add nav component */}
+            {/* FIXME extract logo into separate component */}
+            <div className="font-bold text-4xl md:text-5xl lg:text-6xl leading-none tracking-tight p-4 bg-gray-800 text-white text-3xl font-bold">
+              Know<span className="text-primary-500 font-extrabold">ii</span>
+            </div>
+            <div className="knowii-content-wrapper">{children}</div>
+            {/* FIXME add footer */}
+            <div>Footer</div>
+          </PrimeReactProvider>
+        </main>
       </body>
     </html>
   );
