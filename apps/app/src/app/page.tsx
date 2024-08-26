3f67d7b39769607dc3d7643b8252a5a815d30f0f
Sébastien Dubois <sebastien.dubois@nbb.be>
2024-08-26 15:08:19 +0200

    chore(all): updated styles

--- apps/app/src/app/page.tsx
+++ apps/app/src/app/page.tsx
@@ -2,9 +2,10 @@
 
 export default function Index() {
   return (
-    <div className="w-full">
-      <h1 className="p-6 bg-gray-800 text-white text-3xl font-bold">
-        &nbsp;Know<span class="text-primary-500 font-bold">ii</span>
+    <div className="full-page">
+      {/* Turn Knowii logo into a component */}
+      <h1 className="p-4 bg-gray-800 text-white text-3xl font-bold">
+        &nbsp;Know<span className="text-primary-500 font-extrabold">ii</span>
       </h1>
 
       <div className="mt-4 p-4 flex flex-col lg:flex-row w-full">Hello world</div>
