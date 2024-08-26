ebd716e1694facee42baa77dff735f3cc1927e33
Sébastien Dubois <sebastien.dubois@nbb.be>
2024-08-26 10:49:44 +0200

    chore(all): removed old app

--- apps/knowii/pages/api/trpc/[trpc].ts
+++
@@ -1,9 +0,0 @@
-import * as trpcNext from '@trpc/server/adapters/next';
-import { trpcAppRouter } from '@knowii/server';
-
-// export API handler
-// @see https://trpc.io/docs/api-handler
-export default trpcNext.createNextApiHandler({
-  router: trpcAppRouter,
-  createContext: () => ({}),
-});
