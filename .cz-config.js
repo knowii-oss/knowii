89f4db3ec7925ce48dfb66e08e6e857b764cfb69
Sébastien Dubois <sebastien.dubois@nbb.be>
2024-08-26 11:18:49 +0200

    chore(all): updated eslint config

--- .cz-config.js
+++ .cz-config.js
@@ -17,9 +17,18 @@ module.exports = {
       value: 'style',
       name: 'style:    Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)',
     },
-    { value: 'refactor', name: 'refactor: A code change that neither fixes a bug nor adds a feature' },
-    { value: 'perf', name: 'perf:     A code change that improves performance' },
-    { value: 'test', name: 'test:     Adding missing tests or correcting existing tests' },
+    {
+      value: 'refactor',
+      name: 'refactor: A code change that neither fixes a bug nor adds a feature',
+    },
+    {
+      value: 'perf',
+      name: 'perf:     A code change that improves performance',
+    },
+    {
+      value: 'test',
+      name: 'test:     Adding missing tests or correcting existing tests',
+    },
     {
       value: 'build',
       name: 'build:     Changes that affect the build system or external dependencies (example scopes: rollup, npm)',
