2b64a9719bdc1ee9400549111d326f45feaba1ea
Sébastien Dubois <sebastien.dubois@nbb.be>
2024-08-26 10:53:01 +0200

    chore(all): removed prisma. Cfr #596

--- prisma/migrations/20230213135432_no_default_name_for_communities/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."communities" ALTER COLUMN "name" DROP DEFAULT;
--- prisma/migrations/20230403105819_added_resources_up_and_down_votes/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."resources" ADD COLUMN     "down_votes" INTEGER NOT NULL DEFAULT 0,
-ADD COLUMN     "up_votes" INTEGER NOT NULL DEFAULT 0;
