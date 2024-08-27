1e407cd6cf4061f2a1b0fc51f89b681366f1a272
Sébastien Dubois <sebastien.dubois@nbb.be>
2024-08-27 09:55:29 +0200

    chore(code): added server supabase utils

--- libs/server/src/lib/server.ts
+++ libs/server/src/lib/server.ts
@@ -1,3 +1,7 @@
 export function server(): string {
   return 'server';
 }
+
+// Supabase
+export * from './supabase/supabase-admin';
+export * from './supabase/get-internal-user-id-from-supabase-session.fn';
