c151592b4e5b22cee623c43bde979215f8bcfe53
Sébastien Dubois <sebastien.dubois@nbb.be>
2024-08-27 10:16:20 +0200

    chore(all): added stripe client

--- libs/client/src/lib/client.ts
+++ libs/client/src/lib/client.ts
@@ -1,3 +1,6 @@
 export function client(): string {
   return 'client';
 }
+
+// Stripe
+export * from './stripe/stripe-client.fn';
---
+++ libs/client/src/lib/stripe/stripe-client.fn.ts
@@ -0,0 +1,11 @@
+import { loadStripe, Stripe } from '@stripe/stripe-js';
+
+let stripePromise: Promise<Stripe | null>;
+
+export function getStripe() {
+  if (!stripePromise) {
+    stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY ?? '');
+  }
+
+  return stripePromise;
+}
