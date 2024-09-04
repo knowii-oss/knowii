c8d3b2c8da669f9e5352ab7b0627d9c2b4e03ef3
Sebastien Dubois <seb@dsebastien.net>
2024-09-04 07:57:34 +0200

    chore(ui): extracted guest footer

---
+++ apps/knowii/src/Components/FooterGuest.tsx
@@ -0,0 +1,7 @@
+export default function FooterGuest() {
+  return (
+    <div className="mt-4 py-2 text-center text-gray-600 text-sm">
+      <span>© Knowii. All rights reserved.</span>
+    </div>
+  );
+}
