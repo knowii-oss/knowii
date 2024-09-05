1cb21e3bd74df34a35a1b0d4758762ab626125bf
Sebastien Dubois <seb@dsebastien.net>
2024-09-05 07:51:45 +0200

    feat(ui): added the possibility to customize the app logo

--- apps/knowii/src/Components/ApplicationLogo.tsx
+++ apps/knowii/src/Components/ApplicationLogo.tsx
@@ -1,8 +1,12 @@
-// TODO add support for passing: { className }: { className?: string }
-// TODO replace by SVG logo -->
-export default function ApplicationLogo() {
+import classNames from 'classnames';
+
+interface Props {
+  className?: string;
+}
+
+export default function ApplicationLogo(props: Props) {
   return (
-    <div className="text-5xl md:text-6xl lg:text-8xl tracking-wide min-h-full">
+    <div className={classNames('text-5xl md:text-6xl lg:text-8xl tracking-wide min-h-full', props.className)}>
       <span className="font-bold text-white">Know</span>
       <span className="text-primary-500 font-extrabold">ii</span>
     </div>
