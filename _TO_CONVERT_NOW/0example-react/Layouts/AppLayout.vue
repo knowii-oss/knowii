028234a6dd43bfec433582c817834488507e0fa4
Sebastien Dubois <seb@dsebastien.net>
2024-09-05 12:34:10 +0200

    feat(ui): implemented based menu styling. Closes #683

--- _TO_CONVERT_NOW/0example-react/Layouts/AppLayout.vue
+++ _TO_CONVERT_NOW/0example-react/Layouts/AppLayout.vue
@@ -1,23 +1,3 @@
-<template>
-  <div>
-    <div class="min-h-screen bg-gray-100">
-      <nav class="bg-white border-b border-gray-100">
-        <!-- Primary Navigation Menu -->
-        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div class="flex justify-between h-16">
-            <div class="flex">
-              <!-- Logo -->
-              <div class="shrink-0 flex items-center">
-                <Link :href="route('dashboard')">
-                  <ApplicationMark class="block h-9 w-auto" />
-                </Link>
-              </div>
-
-              <!-- Navigation Links -->
-              <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                <NavLink :href="route('dashboard')" :active="route().current('dashboard')"> Dashboard </NavLink>
-              </div>
-            </div>
 
             <div class="hidden sm:flex sm:items-center sm:ms-6">
               <!-- Settings Dropdown -->
