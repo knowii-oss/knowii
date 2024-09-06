d30e22bd4c17a506f2470348248bba7952e84dd9
Sebastien Dubois <seb@dsebastien.net>
2024-09-06 15:04:41 +0200

    fix(all): fixed bug with the exception handler. Needs to handle redirect responses

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,7 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use \Illuminate\Http\RedirectResponse;
 use Inertia\Inertia;
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -32,7 +33,7 @@ return Application::configure(basePath: dirname(__DIR__))
   // https://inertiajs.com/error-handling
   // https://laravel.com/docs/11.x/errors#handling-exceptions
   ->withExceptions(function (Exceptions $exceptions) {
-    $exceptions->respond(function (Response $response, Throwable $exception, Request $request) {
+    $exceptions->respond(function (Response | RedirectResponse $response, Throwable $exception, Request $request) {
       if (!app()->environment(['local', 'testing']) && in_array($response->getStatusCode(), [500, 503, 404, 403])) {
         return Inertia::render('Error', ['status' => $response->getStatusCode()])->toResponse($request)->setStatusCode($response->getStatusCode());
       } elseif ($response->getStatusCode() === 419) {
