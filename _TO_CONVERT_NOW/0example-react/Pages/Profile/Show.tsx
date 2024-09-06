45c73fb22a837999e2fbdc231e4d36519c6bd76c
Sebastien Dubois <seb@dsebastien.net>
2024-09-06 16:11:24 +0200

    chore(all): removed some more code to migrate

--- _TO_CONVERT_NOW/0example-react/Pages/Profile/Show.tsx
+++ _TO_CONVERT_NOW/0example-react/Pages/Profile/Show.tsx
@@ -1,28 +1,8 @@
-
-
-          {jetstream.canManageTwoFactorAuthentication && (
-            <>
-              <div className="mt-10 sm:mt-0">
-                <TwoFactorAuthenticationForm requiresConfirmation={confirmsTwoFactorAuthentication} />
-              </div>
-              <SectionBorder />
-            </>
-          )}
-
-          <div className="mt-10 sm:mt-0">
-            <LogoutOtherBrowserSessionsForm sessions={sessions} />
-          </div>
-
-          {jetstream.hasAccountDeletionFeatures && (
-            <>
-              <SectionBorder />
-              <DeleteUserForm className="mt-10 sm:mt-0" />
-            </>
-          )}
-        </div>
-      </div>
-    </AppLayout>
+{
+  jetstream.hasAccountDeletionFeatures && (
+    <>
+      <SectionBorder />
+      <DeleteUserForm className="mt-10 sm:mt-0" />
+    </>
   );
-};
-
-export default Profile;
+}
