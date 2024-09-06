f1c8a12d0652f22d88bdab40293f6b4c91c8201e
Sebastien Dubois <seb@dsebastien.net>
2024-09-06 14:03:54 +0200

    chore(ui): created separator component

--- apps/knowii/src/Components/SectionBorder.tsx
+++ apps/knowii/src/Components/SectionBorder.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export default function SectionBorder() {
   return (
     <div className="hidden sm:block">
---
+++ apps/knowii/src/Components/Separator.tsx
@@ -0,0 +1,3 @@
+export default function Separator() {
+  return <hr className="my-8 h-px shadow border-0 bg-gray-100 " />;
+}
