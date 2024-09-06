3da8b297c0e54408174cf66f4c13aa9511d042b8
Sebastien Dubois <seb@dsebastien.net>
2024-09-06 10:04:54 +0200

    chore(all): only show the error page in non-local environments

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -33,7 +33,7 @@ return Application::configure(basePath: dirname(__DIR__))
   // https://laravel.com/docs/11.x/errors#handling-exceptions
   ->withExceptions(function (Exceptions $exceptions) {
     $exceptions->respond(function (Response $response, Throwable $exception, Request $request) {
-      if (app()->environment(['local', 'testing']) && in_array($response->getStatusCode(), [500, 503, 404, 403])) {
+      if (!app()->environment(['local', 'testing']) && in_array($response->getStatusCode(), [500, 503, 404, 403])) {
         return Inertia::render('Error', ['status' => $response->getStatusCode()])->toResponse($request)->setStatusCode($response->getStatusCode());
       } elseif ($response->getStatusCode() === 419) {
         return back()->with([
