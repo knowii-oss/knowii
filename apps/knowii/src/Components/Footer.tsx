2e2bf5c01fcf9efbfc8749402b95727047ef3f3c
Sebastien Dubois <seb@dsebastien.net>
2024-09-07 08:27:56 +0200

    feat(ui): added footer for desktop

--- apps/knowii/src/Components/Footer.tsx
+++ apps/knowii/src/Components/Footer.tsx
@@ -1,3 +1,70 @@
+import { metadata, PRIVACY_POLICY_URL, TERMS_OF_SERVICE_URL } from '@knowii/common';
+import ApplicationLogo from '@/Components/ApplicationLogo';
+import { Button } from 'primereact/button';
+import { FaEnvelope, FaGithub } from 'react-icons/fa';
+import { FaX } from 'react-icons/fa6';
+import { Link } from '@inertiajs/react';
+
 export default function Footer() {
-  return <div className="mt-4 py-2 text-center text-gray-600 text-sm">TODO</div>;
+  return (
+    <footer className="page-content-boundaries bg-gray-800 grid grid-cols-3 gap-8 min-h-[16rem] rounded-t-md p-4 text-white pt-8">
+      {/* first part */}
+      <div className="flex flex-col gap-4 items-center">
+        <ApplicationLogo />
+        <div className="flex flex-row gap-4 justify-center">
+          <a href={`mailto:${metadata.contact.info}`}>
+            <Button aria-label="Contact Knowii">
+              <FaEnvelope />
+            </Button>
+          </a>
+          <a className="" href={metadata.social.xSebastien}>
+            <Button severity="secondary" aria-label="dSebastien on X">
+              <FaX />
+            </Button>
+          </a>
+        </div>
+        <div>
+          <span className="text-xs">© {metadata.author}. All rights reserved</span>
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-2 items-center">
+        <div className="mb-3 text-xl font-bold mt-4 w-full">Links</div>
+        <ul className="text-sm w-full">
+          <li className="flex flex-row gap-2 items-center">
+            <a className="hover:text-primary-500" href={metadata.sourceCodeUrl}>
+              Source code
+            </a>
+            <FaGithub />
+          </li>
+          <li>
+            <a className="hover:text-primary-500" href={metadata.social.newsletterSebastien}>
+              Newsletter
+            </a>
+          </li>
+          <li>
+            <a className="hover:text-primary-500" href={metadata.social.blogSebastien}>
+              Blog
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div className="flex flex-col gap-2 items-center">
+        <div className="mb-3 text-xl font-bold mt-4 w-full">Legal</div>
+        <ul className="text-sm w-full">
+          <li>
+            <Link className="hover:text-primary-500" href={route(TERMS_OF_SERVICE_URL)}>
+              Terms of service
+            </Link>
+          </li>
+          <li>
+            <Link className="hover:text-primary-500" href={route(PRIVACY_POLICY_URL)}>
+              Privacy policy
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
 }
--- apps/knowii/src/Components/PageContentWrapper.tsx
+++ apps/knowii/src/Components/PageContentWrapper.tsx
@@ -1,7 +1,5 @@
 import { PropsWithChildren } from 'react';
 
 export default function PageContentWrapper(props: PropsWithChildren) {
-  return (
-    <main className="mt-8 md:mt-12 lg:mt-16 sm:mx-4 md:mx-8 lg:mx-16 xl:mx-24 min-h-[32rem] xl:min-h-[48rem] px-4">{props.children}</main>
-  );
+  return <main className="page-content-boundaries min-h-[32rem] xl:min-h-[48rem]">{props.children}</main>;
 }
