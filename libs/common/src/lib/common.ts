c84dd10dfe5b9b65bed1c1efa28393044cf7755b
Sebastien Dubois <seb@dsebastien.net>
2024-09-07 08:27:31 +0200

    chore(all): added metadata json file

--- libs/common/src/lib/common.ts
+++ libs/common/src/lib/common.ts
@@ -8,3 +8,5 @@ export * from './constants';
 export * from './types/jetstream/jetstream-inertia.intf';
 export * from './hooks/use-debounce.hook.fn';
 export * from './hooks/use-typed-page.hook.fn';
+
+export { default as metadata } from './metadata.json';
