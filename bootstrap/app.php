19f68d63362e2b9739c10530ffbc45f95c1a12d0
Sebastien Dubois <seb@dsebastien.net>
2024-09-10 14:24:44 +0200

    chore(all): accepted JsonResponse in the global exception handler

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -33,7 +33,7 @@ return Application::configure(basePath: dirname(__DIR__))
   // https://inertiajs.com/error-handling
   // https://laravel.com/docs/11.x/errors#handling-exceptions
   ->withExceptions(function (Exceptions $exceptions) {
-    $exceptions->respond(function (Response | RedirectResponse $response, Throwable $exception, Request $request) {
+    $exceptions->respond(function (Response | RedirectResponse | JsonResponse $response, Throwable $exception, Request $request) {
       if (!app()->environment(['local', 'testing']) && in_array($response->getStatusCode(), [500, 503, 404, 403])) {
         return Inertia::render('Error', ['status' => $response->getStatusCode()])->toResponse($request)->setStatusCode($response->getStatusCode());
       } elseif ($response->getStatusCode() === 419) {
