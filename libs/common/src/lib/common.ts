7da6ef112d66da3eca88ac870aa01360bf5889c3
Sebastien Dubois <seb@dsebastien.net>
2024-09-11 06:50:20 +0200

    chore(all): added http status enum

--- libs/common/src/lib/common.ts
+++ libs/common/src/lib/common.ts
@@ -7,6 +7,7 @@ export * from './constants';
 
 export * from './types/jetstream/jetstream-inertia.intf';
 export * from './types/communities.intf';
+export * from './types/http-status.intf';
 export * from './hooks/use-debounce.hook.fn';
 export * from './hooks/use-typed-page.hook.fn';
 
