e2b3eabfd05ef9657a2b9f7e4066afdc49e31087
Sebastien Dubois <seb@dsebastien.net>
2024-09-12 12:12:07 +0200

    chore(ui): improved the welcome page

--- apps/knowii/src/Pages/Welcome.tsx
+++ apps/knowii/src/Pages/Welcome.tsx
@@ -35,7 +35,7 @@ export default function Welcome(props: WelcomePageProps) {
         <PageContentWrapper>
           <div className="flex flex-col items-center">
             <h1 className="text-primary-500">
-              Welcome&nbsp;
+              Coming soon&nbsp;
               <span role={'img'} aria-label="waving hand">
                 👋
               </span>
@@ -43,9 +43,15 @@ export default function Welcome(props: WelcomePageProps) {
             <h2 className="mt-4">
               Know<span className="text-primary-500">ii</span> is a place for your community's Knowledge, Ideas and Inspiration.
             </h2>
+            <div className="mt-8 text-2xl">Get ready for a revolutionary knowledge-sharing experience!</div>
           </div>
 
-          <div className="mt-16 flex flex-row justify-center text-5xl font-bold">Coming soon...</div>
+
+          {/*<div className="mt-16 flex flex-col items-center mt-8">*/}
+          {/*  <button className="px-6 py-2 bg-primary-500 text-white rounded-full hover:bg-primary-600 transition-colors duration-300">*/}
+          {/*    Join the waitlist*/}
+          {/*  </button>*/}
+          {/*</div>*/}
         </PageContentWrapper>
         <Footer />
       </PageWrapper>
