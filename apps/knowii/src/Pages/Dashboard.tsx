08bbdaa917c8c48405e862c3e0ec4bd259d3d853
Sebastien Dubois <seb@dsebastien.net>
2024-09-12 12:06:40 +0200

    chore(ui): improve page title rendering

--- apps/knowii/src/Pages/Dashboard.tsx
+++ apps/knowii/src/Pages/Dashboard.tsx
@@ -2,7 +2,7 @@ import AppLayout from '@/Layouts/AppLayout';
 
 export default function Dashboard() {
   return (
-    <AppLayout title="Dashboard" header={<h1 className="text-xl font-semibold leading-tight text-white">Dashboard</h1>}>
+    <AppLayout title="Dashboard" pageTitle="Dashboard">
       <div className="flex flex-col items-center">Welcome to your personal Dashboard</div>
     </AppLayout>
   );
