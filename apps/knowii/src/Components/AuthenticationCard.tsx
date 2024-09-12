bbb6aea9893d7847d50581e878b2c8890b73fa6d
Sebastien Dubois <seb@dsebastien.net>
2024-09-12 10:41:44 +0200

    fix(ui): removed margin top for login pages on mobile
    ISSUES CLOSED: #738

--- apps/knowii/src/Components/AuthenticationCard.tsx
+++ apps/knowii/src/Components/AuthenticationCard.tsx
@@ -1,9 +1,9 @@
-import React, { PropsWithChildren } from 'react';
+import { PropsWithChildren } from 'react';
 import AuthenticationCardLogo from '@/Components/AuthenticationCardLogo';
 
 export default function AuthenticationCard({ children }: PropsWithChildren<Record<string, unknown>>) {
   return (
-    <div className="mt-16 flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
+    <div className="mt-0 pt-0 sm:mt-16 flex flex-col sm:justify-center items-center">
       <div className="bg-gray-800 w-full sm:max-w-md sm:rounded-lg flex justify-center">
         <AuthenticationCardLogo />
       </div>
