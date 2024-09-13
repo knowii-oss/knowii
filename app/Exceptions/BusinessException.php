b35d7057e1cf2a72ef1732c4df5a5d0a864cb013
Sebastien Dubois <seb@dsebastien.net>
2024-09-13 13:18:35 +0200

    chore(all): added base exception hierarchy for the back-end

---
+++ app/Exceptions/BusinessException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Exceptions;
+
+/**
+ * Thrown when there are business issues (e.g., name already taken, business rule violation, etc).
+ */
+class BusinessException extends KnowiiException
+{
+
+}
---
+++ app/Exceptions/TechnicalException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Exceptions;
+
+/**
+ * Thrown when there are technical issues.
+ */
+class TechnicalException extends KnowiiException
+{
+
+}
