5e93c7dfc1fbe74218c18a7e6ff4965b966a9a8e
Sebastien Dubois <seb@dsebastien.net>
2024-09-16 10:28:12 +0200

    chore(all): hide the communities property in serialized user objects

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,6 +42,7 @@ class User extends Authenticatable implements MustVerifyEmail
      * @var array<int, string>
      */
     protected $hidden = [
+        'communities',
         'id',
         'password',
         'remember_token',
