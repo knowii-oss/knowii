2bd7f22378222e4db5ff73947c3ab72aaa0c7343
Sebastien Dubois <seb@dsebastien.net>
2024-09-25 12:37:54 +0200

    chore(all): add community resource level enum

---
+++ app/Enums/KnowiiCommunityResourceLevel.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Enums;
+
+enum KnowiiCommunityResourceLevel: string
+{
+  case Beginner = "beginner";
+  case Intermediate = "intermediate";
+  case Advanced = "advanced";
+  case Expert = "expert";
+
+  public static function toCommaSeparatedString(): string
+  {
+    return implode(',', array_column(self::cases(), 'value'));
+  }
+
+  public static function toStringArray(): array
+  {
+    return array_column(self::cases(), 'value');
+  }
+}
