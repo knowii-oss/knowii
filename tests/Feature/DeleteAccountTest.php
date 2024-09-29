487cb005b64cce24582309d6ec1cab44e21f2829
Sebastien Dubois <seb@dsebastien.net>
2024-09-29 14:44:48 +0200

    chore(qa): added tests around user account deletion

--- tests/Feature/DeleteAccountTest.php
+++ tests/Feature/DeleteAccountTest.php
@@ -26,3 +26,41 @@ test('correct password must be provided before account can be deleted', function
 })->skip(function () {
     return ! Features::hasAccountDeletionFeatures();
 }, 'Account deletion is not enabled.');
+
+
+test('deleting a user account removes the user id from the user profile', function () {
+  $this->actingAs($user = User::factory()->withUserProfile()->create());
+
+  $userProfile = $user->profile;
+
+  expect($userProfile->user_id)->not->toBeNull();
+
+  $this->delete('/user', [
+    'password' => 'password',
+  ]);
+
+  expect($userProfile->fresh()->user_id)->toBeNull();
+});
+
+test('deleting a user account does not remove the username and email from the user profile', function () {
+  $this->actingAs($user = User::factory()->withUserProfile()->create());
+
+  $userProfile = $user->profile;
+
+  $initialUsername = $userProfile->username;
+  $initialEmail = $userProfile->email;
+
+  expect($userProfile->username)->not->toBeNull();
+  expect($userProfile->email)->not->toBeNull();
+
+  $this->delete('/user', [
+    'password' => 'password',
+  ]);
+
+  $freshUserProfile = $userProfile->fresh();
+
+  expect($freshUserProfile->username)->not->toBeNull();
+  expect($freshUserProfile->username)->toBe($initialUsername);
+  expect($freshUserProfile->email)->not->toBeNull();
+  expect($freshUserProfile->email)->toBe($initialEmail);
+});
