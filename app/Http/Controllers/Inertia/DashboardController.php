03441f49b305dd25d5129ebfcf6822e3ddfd026c
Sebastien Dubois <seb@dsebastien.net>
2024-10-05 11:06:51 +0200

    chore(all): improved inertia page controllers data serialization using json resources

--- app/Http/Controllers/Inertia/DashboardController.php
+++ app/Http/Controllers/Inertia/DashboardController.php
@@ -2,27 +2,30 @@
 
 namespace App\Http\Controllers\Inertia;
 
-use App\Models\Community;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Routing\Controller;
-use Illuminate\Support\Facades\Gate;
 use Inertia\Response;
 use Laravel\Jetstream\Jetstream;
 use Laravel\Jetstream\RedirectsActions;
 
 class DashboardController extends Controller
 {
-    use RedirectsActions;
+  use RedirectsActions;
 
-    /**
-     * Show the community management screen.
-     *
-     * @param  Request  $request
-     * @return Response
-     */
-    public function show(Request $request): Response
-    {
-        // WARNING: The props passed here must remain aligned with the props expected by the page
-        return Jetstream::inertia()->render($request, 'Dashboard', []);
-    }
+  /**
+   * Show the community management screen.
+   *
+   * @param Request $request
+   * @return Response
+   */
+  public function show(Request $request): Response
+  {
+    // Disable wrapping for the data we return to the frontend from this controller
+    // This lets us use the API Resources classes without the wrapping that is normally applied
+    JsonResource::withoutWrapping();
+
+    // WARNING: The props passed here must remain aligned with the props expected by the page
+    return Jetstream::inertia()->render($request, 'Dashboard', []);
+  }
 }
