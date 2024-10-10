d7359359ecef1a5d2db5d3685af0040417c2edb4
Sebastien Dubois <seb@dsebastien.net>
2024-10-10 16:05:24 +0200

    chore(all): improved markdown conversion

--- app/Traits/HtmlToMarkdown.php
+++ app/Traits/HtmlToMarkdown.php
@@ -19,18 +19,17 @@ trait HtmlToMarkdown
    */
   public function convertHtmlToMarkdown(string $html): string
   {
-    // Configure the converter
-    $environment = new Environment([
-      'strip_tags' => true,
-      'remove_nodes' => 'script style',
-      'hard_break' => true,
-      'preserve_comments' => false,
-    ]);
+    $converter = new HtmlConverter();
+
+    // Configuration
+    $converter->getConfig()->setOption('hard_break', true); // turn <br> into `\n` instead of `  \n`. i.e., GitHub Flavored Markdown (GFM) style
+    $converter->getConfig()->setOption('strip_tags', true); // strip tags that don't have markdown equivalents. N.B. Strips tags, not their content
+    $converter->getConfig()->setOption('remove_nodes', 'meta style script iframe'); // remove specific dom nodes
+    $converter->getConfig()->setOption('preserve_comments', false); // remove comments
+    $converter->getConfig()->setOption('strip_placeholder_links', true); // remove <a> that doesn't have href.
 
     // Add table support
-    $environment->addConverter(new TableConverter());
-
-    $converter = new HtmlConverter($environment);
+    $converter->getEnvironment()->addConverter(new TableConverter());
 
     // Convert
     $markdown = $converter->convert($html);
