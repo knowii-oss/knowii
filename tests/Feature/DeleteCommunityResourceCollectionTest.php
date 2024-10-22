0816b4059a371eebf9787bd91247a1ec2c9e1bd4
Sebastien Dubois <seb@dsebastien.net>
2024-10-22 07:44:02 +0200

    feat(all): added action to delete resource collections

---
+++ tests/Feature/DeleteCommunityResourceCollectionTest.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Contracts\Communities\DeletesCommunities;
+use App\Contracts\CommunityResourceCollections\DeletesCommunityResourceCollections;
+use App\Enums\KnowiiCommunityMemberRole;
+use App\Enums\KnowiiCommunityVisibility;
+use App\Models\Community;
+use App\Models\User;
+
+test('resource collections can be deleted', function () {
+    $this->actingAs($user = User::factory()->withUserProfile()->withPersonalCommunity()->create());
+
+    $community = $user->ownedCommunities()->first();
+
+    expect($community->communityResourceCollections)->toHaveCount(0);
+
+    $collection = $community->communityResourceCollections()->create([
+        'name' => 'Foo',
+        'description' => 'Bar',
+    ]);
+
+    expect($community->fresh()->communityResourceCollections)->toHaveCount(1);
+
+    $deleter = app(DeletesCommunityResourceCollections::class);
+    $deleter->delete($user, $collection);
+
+    expect($community->fresh()->communityResourceCollections)->toHaveCount(0);
+});
