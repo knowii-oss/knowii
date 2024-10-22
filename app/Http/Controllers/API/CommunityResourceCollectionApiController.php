b1022c77be7404e3a50b22eb96de75cb79631da3
Sebastien Dubois <seb@dsebastien.net>
2024-10-22 17:13:59 +0200

    feat(api): added support for deleting resource collections

--- app/Http/Controllers/API/CommunityResourceCollectionApiController.php
+++ app/Http/Controllers/API/CommunityResourceCollectionApiController.php
@@ -2,34 +2,48 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Contracts\CommunityResourceCollections\DeletesCommunityResourceCollections;
 use App\Http\Controllers\Controller;
 use App\Models\Community;
 use App\Contracts\CommunityResourceCollections\CreatesCommunityResourceCollections;
 use App\Http\Resources\CommunityResourceCollectionResource;
+use App\Models\CommunityResourceCollection;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 use App\Traits\ApiResponses;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Log;
 
 class CommunityResourceCollectionApiController extends Controller
 {
-    use ApiResponses;
+  use ApiResponses;
 
-    final public function store(Request $request, Community $community): JsonResponse
-    {
-        Log::info('Processing API request to create a new community resource collection.');
-        Log::debug("User: ", [$request->user()]);
+  final public function store(Request $request, Community $community): JsonResponse
+  {
+    Log::info('Processing API request to create a new community resource collection.');
+    Log::debug("User: ", [$request->user()]);
 
-        // Filter the input to only include the fields that are needed/accepted
-        $input = $request->only(['name', 'description']);
-        Log::debug("Input", [$input]);
+    // Filter the input to only include the fields that are needed/accepted
+    $input = $request->only(['name', 'description']);
+    Log::debug("Input", [$input]);
 
-        $creator = app(CreatesCommunityResourceCollections::class);
-        $createdItem = $creator->create($request->user(), $community, $input);
+    $creator = app(CreatesCommunityResourceCollections::class);
+    $createdItem = $creator->create($request->user(), $community, $input);
 
-        return self::created(
-            new CommunityResourceCollectionResource($createdItem, true),
-            "Resource collection created successfully"
-        );
-    }
+    return self::created(
+      new CommunityResourceCollectionResource($createdItem, true),
+      "Resource collection created successfully"
+    );
+  }
+
+  final public function destroy(Request $request, Community $community, CommunityResourceCollection $communityResourceCollection): Response
+  {
+    Log::info('Processing API request to delete a resource collection.');
+    Log::debug("User: ", [$request->user()]);
+    
+    $deleter = app(DeletesCommunityResourceCollections::class);
+    $deleter->delete($request->user(), $communityResourceCollection);
+
+    return self::deleted();
+  }
 }
--- tests/Feature/CommunityResourceCollectionApiControllerTest.php
+++ tests/Feature/CommunityResourceCollectionApiControllerTest.php
@@ -28,3 +28,31 @@ test('resource collections can be created via the API', function () {
   expect($community->communityResourceCollections()->latest('id')->first()->name)->toEqual('Test');
   expect($community->communityResourceCollections()->latest('id')->first()->description)->toEqual('Awesome collection');
 });
+
+test('resource collections can be deleted via the API', function () {
+  $this->actingAs($user = User::factory()->withUserProfile()->withPersonalCommunity()->create());
+
+  $community = $user->ownedCommunities()->first();
+
+  expect($community->communityResourceCollections)->toHaveCount(0);
+
+  $resourceCollection = $community->communityResourceCollections()->create([
+    'name' => 'Foo',
+    'description' => 'Bar',
+  ]);
+
+  $community = $community->fresh();
+
+  expect($community->communityResourceCollections)->toHaveCount(1);
+
+  // TODO stop hardcoding URLs in tests
+  $requestUrl = 'api/v1/communities/' . $community->cuid . '/resource-collections/' . $resourceCollection->cuid;
+
+  $response = $this->delete($requestUrl, [
+    'Accept' => 'application/json',
+  ]);
+
+  $response->assertStatus(Response::HTTP_NO_CONTENT);
+
+  expect($community->fresh()->communityResourceCollections)->toHaveCount(0);
+});
